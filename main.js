c7c8e0663c5a8bbe2694ea7b9fbadc6083237286
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-27 10:59:49 +0200

    esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+var parola_1 = prompt("Inserisci prima parola")
+var parola_2 = prompt("Inserisci seconda parola")
+
+if (parola_1 > parola_2) {
+    console.log(parola_1);
+} else if (parola_2 > parola_1) {
+    console.log(parola_2);
+}
