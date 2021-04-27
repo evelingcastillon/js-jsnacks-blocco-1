3cf652893541446c7ba9f2eac781c68c550bb0ab
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-27 11:05:11 +0200

    correzione

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var parola_1 = prompt("Inserisci prima parola")
 var parola_2 = prompt("Inserisci seconda parola")
 
-if (parola_1 > parola_2) {
-    console.log(parola_1);
-} else if (parola_2 > parola_1) {
-    console.log(parola_2);
+var n_1 = parola_1.length
+var n_2 = parola_2.length
+
+if (n_1 > n_2) {
+    console.log(parola_1 + " " + parola_2);
+} else if (n_2 > n_1) {
+    console.log(parola_2 + " " + parola_1);
 }
