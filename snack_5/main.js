4e1a399b21abca5634602c99426372a7657edcbe
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-28 12:14:18 +0200

    esercizio completato con for

--- snack_5/main.js
+++ snack_5/main.js
@@ -2,4 +2,5 @@ var numeri = []
 
 for (var i = 0; i < 6; i++) {
     numeri.push = Number(prompt("Inserisci un numero"))
+    document.getElementById("numeri").innerHTML = numeri.push;
 }
---
+++ snack_6/main.js
@@ -0,0 +1,7 @@
+//Stampa il cubo dei primi N numeri,
+//dove N è un numero indicato dall’utente.
+var n = Number(prompt("Inserisci il numero da stampare in cubo"))
+
+for(var i = 1; i <= n; i++) {
+    console.log(Math.pow(i, 3));
+}
