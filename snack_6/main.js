c9fdfd9bd1c0bbd6122babd2c11ca4d924ba02a9
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-28 12:26:35 +0200

    esercizio completato con for e while

--- snack_6/main.js
+++ snack_6/main.js
@@ -2,6 +2,12 @@
 //dove N è un numero indicato dall’utente.
 var n = Number(prompt("Inserisci il numero da stampare in cubo"))
 
-for(var i = 1; i <= n; i++) {
+/* for(var i = 1; i <= n; i++) {
     console.log(Math.pow(i, 3));
+} */
+
+var i = 1;
+while(i <= n) {
+    console.log(Math.pow(i, 3));
+    i++;
 }
