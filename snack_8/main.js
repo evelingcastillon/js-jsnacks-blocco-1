8d22829f1b9c4e3c1085d76f64498f2e9f98fb56
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-29 22:24:08 +0200

    complete snack 8

--- snack_8/main.js
+++ snack_8/main.js
@@ -1,5 +1,12 @@
 //Chiedi un numero di 4 cifre all’utente
 //calcola la somma di tutte le cifre che compongono il numero.
 
-var n = prompt("Inserisci numero di 2 cifre")
+var n = prompt("Inserisci numero di 4 cifre")
+var n_array = n.split("")
+console.log(n_array);
+var somma = 0;
 
+for(var i = 0; i < n_array.length; i++) {
+    somma += Number(n_array[i]);
+}
+console.log(somma);
