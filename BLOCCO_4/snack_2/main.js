9ecf73d84c3e02bb4801cf4564c64f4765b812f9
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-10 12:15:38 +0200

    completato snack_2

---
+++ BLOCCO_4/snack_2/main.js
@@ -0,0 +1,20 @@
+var a = 10;
+var b = 20;
+
+
+
+
+var area = (a * b) / 2 ;
+console.log(area + 'cm');
+
+var b2 = Math.pow(a,2)
+var a2 = Math.pow(b,2)
+console.log(b2, a2);
+
+var c2 = b2 + a2;
+console.log(c2);
+
+var c = Math.sqrt(c2)
+
+var perimetro = c + a + b ;
+console.log(perimetro);
