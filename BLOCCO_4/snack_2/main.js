fb7b67aab5052e0d28a7ae70c4902eb0bb9a4b1a
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-10 12:23:36 +0200

    prova con altri numeri di varibili

--- BLOCCO_4/snack_2/main.js
+++ BLOCCO_4/snack_2/main.js
@@ -1,20 +1,21 @@
-var a = 10;
-var b = 20;
+var a = 14;
+var b = 48;
 
 
 
 
 var area = (a * b) / 2 ;
-console.log(area + 'cm');
+console.log(area, 'cm^2 è area');
 
 var b2 = Math.pow(a,2)
 var a2 = Math.pow(b,2)
 console.log(b2, a2);
 
 var c2 = b2 + a2;
-console.log(c2);
+console.log(c2, " cm al quadrato");
 
 var c = Math.sqrt(c2)
+console.log(c, " cm ipotenusa");
 
 var perimetro = c + a + b ;
-console.log(perimetro);
+console.log(perimetro, ' cm è perimetro');
