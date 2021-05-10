f8920f68c388bb1516294539034ec6d439d12bf5
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-10 13:30:00 +0200

    add object

--- BLOCCO_4/snack_2/main.js
+++ BLOCCO_4/snack_2/main.js
@@ -1,14 +1,13 @@
-var a = 14;
-var b = 48;
+var triangolo = {
+    altezza : 14,
+    base : 48
+}
 
-
-
-
-var area = (a * b) / 2 ;
+var area = (triangolo.altezza * triangolo.base) / 2 ;
 console.log(area, 'cm^2 è area');
 
-var b2 = Math.pow(a,2)
-var a2 = Math.pow(b,2)
+var b2 = Math.pow(triangolo.base,2)
+var a2 = Math.pow(triangolo.altezza,2)
 console.log(b2, a2);
 
 var c2 = b2 + a2;
@@ -17,5 +16,5 @@ console.log(c2, " cm al quadrato");
 var c = Math.sqrt(c2)
 console.log(c, " cm ipotenusa");
 
-var perimetro = c + a + b ;
+var perimetro = c + triangolo.altezza + triangolo.base ;
 console.log(perimetro, ' cm è perimetro');
